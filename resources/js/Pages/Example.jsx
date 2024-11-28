93134bca42e1477359e51efbebafb679a073ec7f
HccCalama001 <hcc.calamasoporte@gmail.com>
2024-11-28 10:39:17 -0300

    Proyecto base Laravel 8 con Inertia y React

---
+++ resources/js/Pages/Example.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppLayout from "../Layouts/AppLayout";
+
+const Example = () => {
+    return (
+        <AppLayout>
+            <h1>Hello, Inertia.js with React!</h1>
+        </AppLayout>
+    );
+};
+
+export default Example;
